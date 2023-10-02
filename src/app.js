9f1abdb7ca109c1c39ef5a18b51f828f77b5a28f
shaunMcelhinney <shaun@vegascity.org>
2023-10-02 16:22:09 +0100

    TEACH-20 basic subscribe messaging allowing settin teacher and student channels and passing through messages between teacher and students

--- src/app.js
+++ src/app.js
@@ -1,11 +1,44 @@
 import { WebSocketServer } from 'ws';
 
 const wss = new WebSocketServer({ port: 3000 });
+const connections = new Map();
 
 wss.on('connection', function connection(ws) {
-    ws.on('message', function message(data) {
-        console.log('received: %s', data);
+
+   ws.on('message', function message(data) {
+
+        let json = JSON.parse(data);
+
+        switch(json.header.type.toLowerCase()){
+            case "subscribe":
+                const channel = json.body.topic;
+                connections.set(channel,ws)
+            break;
+            case "unsubscribe":
+                const remove = json.body.topic;
+                connections.delete(remove)
+            break;
+            case "message":
+                broadcast(json.body.topic, JSON.stringify({typ:"message", data: json.body.message}))
+            break;
+            default:
+                ws.send("Unable to parse message");
+            break;
+        }
+
     });
 
-    ws.send('something');
+    ws.on('close', () => {
+        connections.delete(ws);
+    });
+
 });
+
+
+function broadcast(channel, message) {
+    connections.forEach((ws, key) => {
+    if (key === channel) {
+        ws.send(message);
+    }
+    });
+}
