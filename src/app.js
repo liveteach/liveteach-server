5cc7fba26c4a99811f4d2637329c2c5cf79fef2e
shaunMcelhinney <shaun@vegascity.org>
2023-10-03 14:00:41 +0100

    TEACH-20 return guid for teacher and check guid&wallet on commands asociated with teacher before dispatching ws messages to students

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import { WebSocketServer } from 'ws';
 
 const wss = new WebSocketServer({ port: 3000 });
 const connections = new Map();
+const teachers = new Map();
 
 wss.on('connection', function connection(ws) {
 
@@ -13,16 +14,40 @@ wss.on('connection', function connection(ws) {
             case "subscribe":
                 const channel = json.body.topic;
                 connections.set(channel,ws)
+                //return a Auth guid for teachers
+                if(json.body.topic === "teacher"){
+                    let guid = getGuid()
+                    broadcast(json.body.topic, JSON.stringify({type:"guid", data: guid}))
+                } 
             break;
             case "unsubscribe":
                 const remove = json.body.topic;
                 connections.delete(remove)
             break;
             case "message":
-                broadcast(json.body.topic, JSON.stringify({typ:"message", data: json.body.message}))
+                console.log(json)
+                // check standard Commands if Teacher controlled check the wallet and guid
+                switch (json.body.message) {
+                    case "activate_class":
+                    case "deactivate_class":
+                    case "start_class":
+                    case "end_class":
+                    case "log":
+                        isValidWalletAndGuid(json)
+                    break;
+                    case "join_class":
+                    case "exit_class":
+                        broadcast(json.body.topic, JSON.stringify({type:json.body.message, data: json.body.message, from: json.body.from}))
+                    break;
+                }
             break;
+            case "register":
+                teachers.set(json.body.wallet, json.body.guid)
+                console.log(teachers)
+            break
             default:
                 ws.send("Unable to parse message");
+                console.log(connections)
             break;
         }
 
@@ -41,4 +66,23 @@ function broadcast(channel, message) {
         ws.send(message);
     }
     });
+}
+
+function getGuid() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        const r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16)
+    })
+}
+
+function isValidWalletAndGuid(json){
+    teachers.forEach((guid, wallet) => {
+        console.log(wallet)
+        console.log(guid)
+        console.log(json.body.wallet === wallet)
+        console.log(json.body.guid === guid)
+        if(json.body.wallet === wallet && json.body.guid === guid){
+            broadcast(json.body.topic, JSON.stringify({type:json.body.message, data: json.body.message, from: json.body.from}))
+        }
+    })
 }
